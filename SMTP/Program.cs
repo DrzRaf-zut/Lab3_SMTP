8c0cbc7dad1a4157b4eb5d649b4c914b367ef20f
DrzRaf-zut <47260181+DrzRaf-zut@users.noreply.github.com>
2019-02-01 23:11:55 +0100

    zad3 SMTP

---
+++ SMTP/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SMTP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                SMTPClient smtpClient = new SMTPClient();
+
+                Console.WriteLine("Podaj adres odbiorcy:\n");
+                string recipent = Console.ReadLine();
+                string subject = "PS LAB N2 ZIMA 2018 N2 14B";
+                string content = "Rafał Drzewowski";
+
+                smtpClient.sendEmail(recipent, subject, content);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+    }
+}
---
+++ SMTP/SMTPClient.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Xml;
+
+namespace SMTP
+{
+    class SMTPClient
+    {
+        private string senderServerAddress;
+        private string senderAddress;
+        private string login;
+        private string pass;
+        private int senderPort;
+
+
+        public SMTPClient()
+        {
+            XmlReader xmlReader = XmlReader.Create("App.config");
+            xmlReader.MoveToContent();
+            while (xmlReader.Read())
+            {
+                if (xmlReader.NodeType == XmlNodeType.Element)
+                {
+                    if (xmlReader.Name == "senderServerAddress")
+                        senderServerAddress = xmlReader.ReadElementString();
+                    else if (xmlReader.Name == "senderAddress")
+                        senderAddress = xmlReader.ReadElementString();
+                    else if (xmlReader.Name == "user")
+                        login = xmlReader.ReadElementString();
+                    else if (xmlReader.Name == "password")
+                        pass = xmlReader.ReadElementString();
+                    else if (xmlReader.Name == "senderPort")
+                        senderPort = Int32.Parse(xmlReader.ReadElementString());
+                }
+            }
+        }
+
+        public void sendEmail(string recipent, string subject, string content)
+        {
+            using (TcpClient tcpClient = new TcpClient(senderServerAddress, senderPort))
+            {
+                using (StreamReader sr = new StreamReader(tcpClient.GetStream()))
+                {
+                    using (StreamWriter sw = new StreamWriter(tcpClient.GetStream()))
+                    {
+                        string responseLine = string.Empty;
+                        responseLine = sr.ReadLine();
+
+                        sw.WriteLine("HELO " + senderServerAddress);
+                        sw.Flush();
+                        responseLine = sr.ReadLine();
+                        if(responseLine.Substring(0, 3) != "250")
+                            throw new Exception("Wystapil blad w komunikacji z serwerem.\n" + responseLine);
+
+                        sw.WriteLine("AUTH PLAIN");
+                        sw.Flush();
+                        responseLine = sr.ReadLine();
+                        if (responseLine.Substring(0, 3) != "334")
+                            throw new Exception("Wystapil blad w komunikacji z serwerem.\n" + responseLine);
+
+
+                        sw.WriteLine(System.Convert.ToBase64String(System.Text.Encoding.UTF8.GetBytes("\0" + login + "\0" + pass)));
+                        sw.Flush();
+                        responseLine = sr.ReadLine();
+                        if (responseLine.Substring(0, 3) != "235")
+                            throw new Exception("Wystapil blad w komunikacji z serwerem.\n" + responseLine);
+
+
+                        sw.WriteLine("MAIL FROM: <" + senderAddress + ">");
+                        sw.Flush();
+                        responseLine = sr.ReadLine();
+                        if (responseLine.Substring(0, 3) != "250")
+                            throw new Exception("Wystapil blad w komunikacji z serwerem.\n" + responseLine);
+
+
+                        sw.WriteLine("RCPT TO: <" + recipent + ">");
+                        sw.Flush();
+                        responseLine = sr.ReadLine();
+                        if (responseLine.Substring(0, 3) != "250")
+                            throw new Exception("Wystapil blad w komunikacji z serwerem.\n" + responseLine);
+
+
+                        sw.WriteLine("DATA");
+                        sw.Flush();
+                        responseLine = sr.ReadLine();
+                        if (responseLine.Substring(0, 3) != "354")
+                            throw new Exception("Wystapil blad w komunikacji z serwerem.\n" + responseLine);
+
+                        string message = "Subject: " + subject + "\r\n"
+                                       + "To: " + recipent + "\r\n"
+                                       + "From: " + senderAddress + "\r\n\r\n"
+                                       + content + "\r\n"
+                                       + ".\r\n";
+                        sw.Write(message);
+                        sw.Flush();
+                        responseLine = sr.ReadLine();
+                        if (responseLine.Substring(0, 3) != "250")
+                            throw new Exception("Wystapil blad w komunikacji z serwerem.\n" + responseLine);
+
+
+                        sw.WriteLine("QUIT");
+                        sw.Flush();
+                        responseLine = sr.ReadLine();
+                        if (responseLine.Substring(0, 3) != "221")
+                            throw new Exception("Wystapil blad w komunikacji z serwerem.\n" + responseLine);
+
+                        Console.WriteLine("Wiadomosc zostala pomyslnie wyslana\n");
+                    }
+                }
+            }
+        }
+    }
+}
